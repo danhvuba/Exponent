3a856d3659a852f5ec2c94968a4e3759a19f776d
danhvuba <danhvuba@gmail.com>
2022-10-06 21:21:21 +0300

    bitset

--- main.cpp
+++ main.cpp
@@ -1,8 +1,33 @@
-#include<iostream>
+#include <iostream>
+#include <math.h>
 
 using namespace std;
 
+const int N = 540143;
+
+int func(float x)
+{
+    int k = x / N;
+    cout << k << endl;
+    return (x - (float)k * N);
+}
+
 int main()
 {
-    cout<<"hello"<<endl;
+    int a = 2;
+    // int n = log(840) / log(2);
+    // int res = 1;
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     if ((840 >> i) & 1 != 0)
+    //     {
+    //         res = (res * a) % N;
+    //     }
+    //     a = func(a*a);
+    //     cout << "   " << a << endl;
+    // }
+    // cout << res << endl;
+    float y = 65536;
+    cout << y * y << endl;
 }
